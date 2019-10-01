9a912f587e6616b87ecc4c222cb1d4b5377abc9f
sarnobat <ss401533@gmail.com>
2019-09-30 17:54:07 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,7 @@ PATH=/bin:/usr/bin
 # Not working on Ubuntu 16
 #bash ./releasetools/x86_hdimage.sh 
 
+# Works 2019-09-30 Ubuntu 16
 sudo PATH=/bin:/usr/bin sh build.sh -mi386 -O ../build tools
+# awaiting result
+sudo PATH=/bin:/usr/bin sh build.sh -mi386 -O ../build -U distribution 2>&1 | tee /tmp/minix.log &!
