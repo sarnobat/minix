449ce24bece693f51117eda68752d6593993b7be
sarnobat <ss401533@gmail.com>
2019-09-30 17:14:57 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 # Ubuntu
 PATH=/bin:/usr/bin
-bash ./releasetools/x86_hdimage.sh 
+
+# Not working on Ubuntu 16
+#bash ./releasetools/x86_hdimage.sh 
+
+sudo PATH=/bin:/usr/bin sh build.sh -mi386 -O ../build tools
